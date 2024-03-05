ff64d481b7362ba333cb32b672b09a713551c241
atian155 <atianthony@outlook.com>
2024-03-05 18:12:01 +0200

    Added option to create users in admin panel

--- scripts/admin/index.js
+++ scripts/admin/index.js
@@ -3,39 +3,75 @@ const loggedIn = localStorage.userId ?? 0;
 if (loggedIn != -1) {
   window.location.replace("../user/login.html");
 }
-const users = JSON.parse(localStorage.users ?? "[]");
 
+const addUserBtn = document.getElementById("add_user_btn");
+const signupForm = document.getElementById("signup_form");
+const [signupNameInput, signupEmailInput, signupPasswordInput] = [
+  document.getElementById("signup_name"),
+  document.getElementById("signup_email"),
+  document.getElementById("signup_password"),
+];
 const usersTable = document.getElementById("users_table");
 
+// Events
+addUserBtn.addEventListener("click", () =>
+  signupForm.classList.toggle("hidden")
+);
+signupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createUser(
+    signupNameInput.value,
+    signupEmailInput.value,
+    signupPasswordInput.value
+  );
+});
+
+function createUser(name, email, password) {
+  const users = JSON.parse(localStorage.users ?? "[]");
+  const userObject = {
+    id: (users[users.length - 1]?.id ?? 0) + 1,
+    name: name,
+    email: email,
+    password: password,
+  };
+  localStorage.users = JSON.stringify([...users, userObject]);
+  addRow(userObject);
+}
+
+function addRow(user) {
+  // Row
+  const userDiv = document.createElement("div");
+  userDiv.id = `user_${user.id}`;
+  userDiv.classList.add("user_row");
+
+  // Button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.addEventListener("click", () => deleteUser(user.id));
+
+  userDiv.innerHTML += `<div>${user.id}</div>
+  <div>${user.name}</div>
+  <div>${user.email}</div>
+  <div>${user.password}</div>`;
+  const actionsDiv = document.createElement("div");
+  actionsDiv.append(deleteBtn);
+  userDiv.append(actionsDiv);
+  usersTable.append(userDiv);
+}
+
 function populateUsersTable() {
+  const users = JSON.parse(localStorage.users ?? "[]");
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
-    // Row
-    const userDiv = document.createElement("div");
-    userDiv.id = `user_${user.id}`;
-    userDiv.classList.add("user_row");
-
-    // Button
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.addEventListener("click", () => deleteUser(user.id));
-
-    userDiv.innerHTML += `<div>${user.id}</div>
-    <div>${user.name}</div>
-    <div>${user.email}</div>
-    <div>${user.password}</div>`;
-    const actionsDiv = document.createElement("div");
-    actionsDiv.append(deleteBtn);
-    userDiv.append(actionsDiv);
-    usersTable.append(userDiv);
+    addRow(user);
   }
 }
 
 populateUsersTable();
 
 function deleteUser(id) {
-  const usersNew = JSON.parse(localStorage.users ?? "[]");
+  const users = JSON.parse(localStorage.users ?? "[]");
   document.querySelector(`#user_${id}`).remove();
-  const usersFilter = usersNew.filter((user) => user.id != id);
+  const usersFilter = users.filter((user) => user.id != id);
   localStorage.users = JSON.stringify([...usersFilter]);
 }
