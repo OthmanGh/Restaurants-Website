0910d1638f171034a5bde3e31a5dbc9455187564
atian155 <atianthony@outlook.com>
2024-03-06 13:38:34 +0200

    Minor fixes

--- scripts/admin/index.js
+++ scripts/admin/index.js
@@ -37,14 +37,14 @@ function createUser(name, email, password) {
     favorites: [],
   };
   localStorage.users = JSON.stringify([...users, userObject]);
-  addRow(userObject);
+  addUserRow(userObject);
 }
 
-function addRow(user) {
+function addUserRow(user) {
   const { id, name, email, password, favorites } = user;
   // Row
   const userRow = document.createElement("tr");
-  userRow.id = `user_${user.id}`;
+  userRow.id = `user_${id}`;
   userRow.classList.add("user_row");
 
   // Button
@@ -68,7 +68,7 @@ function populateUsersTable() {
   const users = JSON.parse(localStorage.users ?? "[]");
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
-    addRow(user);
+    addUserRow(user);
   }
 }
 
@@ -83,13 +83,17 @@ function deleteUser(id) {
 
 // Restaurants section
 const restoForm = document.getElementById("resto_form");
-const [restoImage, restoName, restoDesc, restoLocation, restoRating] = [
+const restosTable = document.querySelector("#restos_table tbody");
+const addRestoBtn = document.getElementById("add_resto_btn");
+const [restoImage, restoName, restoDesc, restoLocation] = [
   document.querySelector('.resto_details input[name="image"]'),
   document.querySelector('.resto_details input[name="name"]'),
   document.querySelector('.resto_details textarea[name="description"]'),
   document.querySelector('.resto_details input[name="location"]'),
-  document.querySelector('.resto_details input[name="rating"]'),
 ];
+addRestoBtn.addEventListener("click", () => {
+  restoForm.classList.toggle("hidden");
+});
 
 restoForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -98,7 +102,7 @@ restoForm.addEventListener("submit", (e) => {
     restoName.value,
     restoDesc.value,
     restoLocation.value,
-    restoRating.value
+    document.querySelector('.resto_details input[name="rating"]:checked').value
   );
 });
 
@@ -114,4 +118,47 @@ function createResto(image, name, desc, location, rating) {
     menu: [],
   };
   localStorage.restos = JSON.stringify([...restos, restoObject]);
+  addRestoRow(restoObject);
+}
+
+function populateRestosTable() {
+  const restos = JSON.parse(localStorage.restos ?? "[]");
+  for (let i = 0; i < restos.length; i++) {
+    const resto = restos[i];
+    addRestoRow(resto);
+  }
+}
+
+populateRestosTable();
+
+function addRestoRow(resto) {
+  const { id, image, name, desc, location, rating } = resto;
+  // Row
+  const restoRow = document.createElement("tr");
+  restoRow.id = `resto_${id}`;
+  restoRow.classList.add("resto_row");
+
+  // Button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("button");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.addEventListener("click", () => deleteResto(id));
+
+  restoRow.innerHTML += `<td>${id}</td>
+  <td><img src="${image}" width="200"></td>
+  <td>${name}</td>
+  <td>${desc}</td>
+  <td>${location}</td>
+  <td>${rating}/5</td>`;
+  const actionsCol = document.createElement("td");
+  actionsCol.append(deleteBtn);
+  restoRow.append(actionsCol);
+  restosTable.append(restoRow);
+}
+
+function deleteResto(id) {
+  const restos = JSON.parse(localStorage.restos ?? "[]");
+  document.querySelector(`#resto_${id}`).remove();
+  const restosFilter = restos.filter((resto) => resto.id != id);
+  localStorage.restos = JSON.stringify([...restosFilter]);
 }
